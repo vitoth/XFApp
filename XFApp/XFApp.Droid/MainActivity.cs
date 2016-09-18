962d50ce0df995150281b26f4a2d1fcde01628cd
vitoth <viktor965@gmail.com>
2016-09-18 16:47:52 +0200

    10x10 board

--- XFApp/XFApp.Droid/MainActivity.cs
+++ XFApp/XFApp.Droid/MainActivity.cs
@@ -9,16 +9,17 @@ using Android.OS;
 
 namespace XFApp.Droid
 {
-	[Activity (Label = "XFApp", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity (Label = "XFApp", Icon = "@drawable/icon", MainLauncher = true, Theme = "@android:style/Theme.Holo.Light", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-			global::Xamarin.Forms.Forms.Init (this, bundle);
-			LoadApplication (new XFApp.App ());
-		}
-	}
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            LoadApplication(new App()); // method is new in 1.3
+        }
+    }
 }
 
